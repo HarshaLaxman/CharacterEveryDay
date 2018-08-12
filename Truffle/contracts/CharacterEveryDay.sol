94258046afe4cd4d65f0af8ccc6da3bc58e7e97b
Harsha Laxman <laxmanharsha.n@gmail.com>
2018-08-12 16:59:03 -0700

    Added timeTravel | 10 passing tests | Changed winning logic

--- Truffle/contracts/CharacterEveryDay.sol
+++ Truffle/contracts/CharacterEveryDay.sol
@@ -5,27 +5,36 @@ contract CharacterEveryDay {
     
     event CharacterSelected(uint8 _character, uint64[256] characterVotes, uint64 day);
     
-    uint64[256] characterVotes;
+    uint64[256] public characterVotes;
     
-    uint64 day; 
-    uint256 timeOfLastVote;
+    uint8 public winningCharacter;
+    uint64 public day; 
+    uint256 public winningCharacterVotes;
+    uint256 public timeOfLastVote;
     
     mapping (uint256 => bool) addressVotedToday;
     
     constructor (uint8 _character) public {
         day = 0;
+        winningCharacterVotes = 0;
         timeOfLastVote = now;
-        addCharacter(_character);
+        voteForCharacter(_character);
     }
     
-     function voteForCharacter(uint8 _character) public {
+    function voteForCharacter(uint8 _character) public {
         if ((now - timeOfLastVote) > 1 days) {
-            determineWinningCharacter();
+            addCharacter(winningCharacter);
         }
         
+        require(_character <= 256);
         uint256 todaysAddressHash = uint(keccak256(abi.encodePacked(day, msg.sender)));
         require(addressVotedToday[todaysAddressHash] == false);
+        
         characterVotes[_character]++;
+        if (characterVotes[_character] > winningCharacterVotes) {
+            winningCharacter = _character;
+            winningCharacterVotes = characterVotes[_character];
+        }
         addressVotedToday[todaysAddressHash] = true;
         timeOfLastVote = now;
     }
@@ -36,20 +45,23 @@ contract CharacterEveryDay {
     
     function addCharacter (uint8 _winningCharacter) private {
         day += uint64((now - timeOfLastVote) / 1 days);
+        // day += 1;
         emit CharacterSelected(_winningCharacter, characterVotes, day);
         delete characterVotes;
+        delete winningCharacter;
+        delete winningCharacterVotes;
     }
     
-    function determineWinningCharacter() private {
-        uint8 winningCharacter = 46; //default if no one voted is '.'
-        uint256 winningCharacterVotes = 0;
-        for (uint8 character = 0; character < characterVotes.length; character++) {
-            if (characterVotes[character] > winningCharacterVotes) {
-                winningCharacterVotes = characterVotes[character];
-                winningCharacter = character;
-            }
-        }
-        addCharacter(winningCharacter);
-    }
+    // function determineWinningCharacter() private {
+    //     uint8 winningCharacter = 46; //default if no one voted is '.'
+    //     uint256 winningCharacterVotes = 0;
+    //     // for (uint8 character = 0; character < characterVotes.length; character++) {
+    //     //     if (characterVotes[character] > winningCharacterVotes) {
+    //     //         winningCharacterVotes = characterVotes[character];
+    //     //         winningCharacter = character;
+    //     //     }
+    //     // }
+    //     addCharacter(winningCharacter);
+    // }
     
 }
--- Truffle/test/TestCharacterEveryDay.js
+++ Truffle/test/TestCharacterEveryDay.js
@@ -1,6 +1,8 @@
 const cED = artifacts.require("CharacterEveryDay");
+const timeTravel = require("./helpers/truffleTimeTravel.js");
 
 contract('TestCharacterEveryDay', async (accounts) => {
+
 	it("should be alive", async () => {
 		let instance = await cED.deployed();
 		let characterVotes = await instance.getCharacterVotes.call();
@@ -8,23 +10,30 @@ contract('TestCharacterEveryDay', async (accounts) => {
 		assert.equal(characterVotes[0].valueOf(), 0);
 	});
 
+	it("should start at day 0", async () => {
+		let instance = await cED.deployed();
+		let day = await instance.day();
+
+		assert.equal(day.valueOf(), 0);
+	});
+
 	it("should record a vote", async () => {
 		let instance = await cED.deployed();
-		let H_Character = 'H'.charCodeAt(0);
+		let A_Character = 'A'.charCodeAt(0);
 
-		await instance.voteForCharacter(H_Character, {from: accounts[0]});
-		let characterVotes = await instance.getCharacterVotes.call();
+		await instance.voteForCharacter(A_Character, {from: accounts[1]});
 
-		assert.equal(characterVotes[H_Character].valueOf(), 1);
+		let A_Votes = await instance.characterVotes(A_Character);
+		assert.equal(A_Votes, 1);
 	});
 
 	it("should not allow a double vote by the same address", async () => {
 		let err = null
 		let instance = await cED.deployed();
-		let L_Character = 'L'.charCodeAt(0);
+		let B_Character = 'B'.charCodeAt(0);
 
 		try {
-			await instance.voteForCharacter(L_Character, {from: accounts[0]});
+			await instance.voteForCharacter(B_Character, {from: accounts[1]});
 		}catch (error) {
 			err = error
 		};
@@ -33,15 +42,29 @@ contract('TestCharacterEveryDay', async (accounts) => {
 
 	it("should record multiple voters' votes", async () => {
 		let instance = await cED.deployed();
-		let H_Character = 'H'.charCodeAt(0);
-		let L_Character = 'L'.charCodeAt(0);
+		let A_Character = 'A'.charCodeAt(0);
+		let B_Character = 'B'.charCodeAt(0);
 
-		await instance.voteForCharacter(H_Character, {from: accounts[1]});
-		await instance.voteForCharacter(L_Character, {from: accounts[2]});
+		await instance.voteForCharacter(A_Character, {from: accounts[2]});
+		await instance.voteForCharacter(B_Character, {from: accounts[3]});
 		let characterVotes = await instance.getCharacterVotes.call();
 
-		assert.equal(characterVotes[H_Character].valueOf(), 2);
-		assert.equal(characterVotes[L_Character].valueOf(), 1);
+		assert.equal(characterVotes[A_Character].valueOf(), 2);
+		assert.equal(characterVotes[B_Character].valueOf(), 1);
 	});
+
+	it("should reset vote permissions after a day", async () => {
+		let C_Character = 'C'.charCodeAt(0);
+		let thirtyHours = 108000; //seconds
+		await timeTravel.advanceTimeAndBlock(thirtyHours);
+		let instance = await cED.deployed();
+
+		await instance.voteForCharacter(C_Character, {from: accounts[1]});
+		let day = await instance.day();
+		
+		assert.equal(day.valueOf(), 1);		
+	});
+
+	
 
 })
